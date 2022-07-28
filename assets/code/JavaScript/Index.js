5dcd7367ad6e09995080bde875c2146f9cb9e46b
DarkSpine <darkspine.33@gmail.com>
2022-07-29 00:13:57 +0200

    update

--- assets/code/JavaScript/Index.js
+++ assets/code/JavaScript/Index.js
@@ -3,16 +3,16 @@ window.onload = function(){
     document.querySelector(".preload").style.display = "none";
 };
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-10vh";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-10vh";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 if(window.innerWidth <= 991){
     // load mobile script
     loadScriptFile('https://files.finsweet.com/webflowhacks/hack27-mobile.js');
